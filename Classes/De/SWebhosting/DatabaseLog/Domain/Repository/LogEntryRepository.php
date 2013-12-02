80a033c2f6444bf098c933d8e600a2fcf2c15c9c
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-12-02 22:29:15 +0100

    [FEATURE] Improved log entry repo features
    Added method to find all log entries of a certain type of a user and a
    method for deleting all log entries of a user.

--- Classes/De/SWebhosting/DatabaseLog/Domain/Repository/LogEntryRepository.php
+++ Classes/De/SWebhosting/DatabaseLog/Domain/Repository/LogEntryRepository.php
@@ -20,4 +20,46 @@ use TYPO3\Flow\Annotations as FLOW3;
  * @FLOW3\Scope("singleton")
  */
 class LogEntryRepository extends \TYPO3\Flow\Persistence\Repository {
+
+	/**
+	 * Finds a log entry by user and message. The latest entries are on the top and
+	 * no limit is applied by default.
+	 *
+	 * @param \TYPO3\Party\Domain\Model\AbstractParty $user the user to search for
+	 * @param string $message the message to search for
+	 * @param array $orderings the order of the log messages
+	 * @param int $limit limit the number of results
+	 * @return \TYPO3\Flow\Persistence\QueryResultInterface
+	 */
+	public function findByUserAndMessage($user, $message, $limit = NULL, $orderings = array('dateTime' => \TYPO3\Flow\Persistence\QueryInterface::ORDER_DESCENDING)) {
+
+		$query = $this->createQuery();
+
+		$query->matching(
+			$query->logicalAnd(
+				$query->equals('user', $user),
+				$query->equals('message', $message)
+			)
+		);
+
+		$query->setOrderings($orderings);
+
+		if (isset($limit)) {
+			$query->setLimit($limit);
+		}
+
+		return $query->execute();
+	}
+
+	/**
+	 * Removes all log entries from the given user
+	 *
+	 * @param \TYPO3\Party\Domain\Model\AbstractParty $user
+	 */
+	public function removeAllFromUser($user) {
+		$userLogEntries = $this->findByUser($user);
+		foreach ($userLogEntries as $logEntry) {
+			$this->remove($logEntry);
+		}
+	}
 }
